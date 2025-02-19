bdef14f9b75bbbaa4463af7f225a1dd9f0f92dc7
mcorreira <megancorreiradev@gmail.com>
2025-02-18 20:15:26 -0500

    add login routing configuration

--- apps/myjeansfinder/src/app/app.routes.ts
+++ apps/myjeansfinder/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  }
+  {}
+];
