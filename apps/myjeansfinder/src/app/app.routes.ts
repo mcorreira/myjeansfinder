89cb7db97f759c35d76deceff7e883fc0ea7362d
mcorreira <megancorreiradev@gmail.com>
2025-02-25 14:16:31 -0500

    Setup Express.js backend with auth and search routes

--- apps/myjeansfinder/src/app/app.routes.ts
+++ apps/myjeansfinder/src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SearchComponent } from './pages/search/search.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadComponent: () =>
-      import('./login/login.component').then((m) => m.LoginComponent),
-  }
-  {}
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
